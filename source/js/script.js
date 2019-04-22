ad5fc9937f0f92af69cf7e06f8594f2f1a4c12d6
Vadim <bychkov.vadim@gmail.com>
2019-04-22 09:03:52 +0300

    Переделал блок index/manual

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,16 @@
 var map;
-var posicionMap = {lat: 59.938985, lng: 30.318867};
-var posicionMarker = {lat: 59.938708, lng: 30.323036};
+var positionMap = {lat: 59.938985, lng: 30.318867};
+var positionMarker = {lat: 59.938708, lng: 30.323036};
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: posicionMap,
+    center: positionMap,
     zoom: 17,
     disableDefaultUI: true,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
   var marker = new google.maps.Marker({
-    position: posicionMarker,
+    position: positionMarker,
     icon: 'img/map-pin.png',
     map: map
   });
