cbe69123badcec451ed0bcf65527c04df92d4ccf
Vadim <bychkov.vadim@gmail.com>
2019-05-27 05:42:02 +0300

    Исправление ошибки запуска сборки

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ gulp.task("server", function () {
   });
 
 gulp.watch("source/sass/**/*.scss", gulp.series("css"));
-gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+gulp.watch("source/img/icon-*.svg", gulp.series("html", "refresh"));
 gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
