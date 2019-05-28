35e4d1cb9c133ff6888ef240a3fe6b0f9b9c6d7c
Vadim <bychkov.vadim@gmail.com>
2019-05-28 09:00:12 +0300

    Исправление Б25. Добавил путь для сохранения неминифицированного style файла

--- gulpfile.js
+++ gulpfile.js
@@ -25,10 +25,12 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
 });
 
 gulp.task("js", function () {
